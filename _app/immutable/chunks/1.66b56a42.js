39814fe2520dae55b0098ae04c0257e53a57af23
Viktor Holta <viktor.holta@outlook.com>
2023-04-03 12:16:15 +0200

    Updates

---
+++ _app/immutable/chunks/1.66b56a42.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.ce93daee.js";export{t as component};
---
+++ _app/immutable/chunks/2.4130c842.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/about-page.svelte.848f131b.js";export{t as component};
