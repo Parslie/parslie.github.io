b13ffba2b0a2995a790ae8548e3476b031d8bfe6
Viktor Holta <viktor.holta@outlook.com>
2023-04-03 12:52:04 +0200

    NEW: initialize svelte remake

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
+<div>
+	<header>
+		<h1>Viktor Holta</h1>
+		<nav>
+			<a href="{base}/">About</a>
+		</nav>
+	</header>
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<h4>Viktor Holta <b>©</b> 2023</h4>
+	</footer>
+</div>
+
+<style lang="scss">
+	
+</style>
