44ae43f494074667ed923ce00898fe0ea2b5ecd0
Viktor Holta <viktor.holta@outlook.com>
2023-11-21 19:24:01 +0100

    NEW: init Article component

---
+++ src/components/Article.svelte
@@ -0,0 +1,37 @@
+<div class="wrapper">
+    <article>
+        <h2>
+            article
+        </h2>
+        <p>
+            bottom text
+        </p>
+    </article>
+</div>
+
+<style lang="scss">
+    $radius: 0.75rem;
+    $border-width: 3px;
+    
+    p {
+        margin: 0;
+    }
+
+    h2 {
+        margin: 0;
+        line-height: 1;
+    }
+
+    .wrapper {
+        padding: $border-width;
+        border-radius: $radius;
+        background: linear-gradient(190deg, $primary, $accent);
+    }
+
+    article {
+        padding: 1rem;
+        
+        border-radius: calc($radius - $border-width);
+        background: $background;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,7 @@
-<article>
-    <h2>An Article</h2>
-    <p>
+<script>
+    import Article from "../components/Article.svelte";
+</script>
 
-        Donec vestibulum aliquet neque, vitae tempor mi sodales quis. Curabitur vel bibendum est, vitae sollicitudin felis. Cras hendrerit bibendum mi sit amet condimentum. Cras consequat metus tellus. Suspendisse ut leo non erat feugiat euismod. Donec ultrices massa a orci rutrum, vel tristique turpis efficitur. Curabitur finibus quis lorem vel facilisis. Mauris sed viverra purus.
-        
-        Nam ac urna tempor, porttitor est sed, sodales lacus. Sed ac nibh eu turpis porta pharetra. Phasellus tempor purus eget nisl ultrices pellentesque. Nulla eget orci et orci placerat tincidunt. Integer ut neque et diam rhoncus ornare. Suspendisse potenti. Pellentesque purus nibh, maximus vel felis eget, mattis condimentum lectus. Praesent eget facilisis nunc. Pellentesque ipsum odio, sollicitudin ac neque ut, hendrerit feugiat mauris. Duis suscipit nulla molestie magna sagittis mattis. Mauris erat justo, dapibus a congue id, tempor mollis est. Integer aliquet, ligula at suscipit ornare, dui nisi fermentum velit, vel tincidunt quam arcu vel lorem. Donec sit amet tincidunt felis. Aenean dui lorem, molestie tempor ultrices eu, feugiat eget ex.
-        
-        Morbi at augue nec nulla tempus semper placerat et nibh. Sed lorem metus, consequat et egestas et, volutpat et libero. Aliquam varius dignissim lobortis. Curabitur posuere mattis mattis. Pellentesque eget eleifend quam. Vestibulum tincidunt nunc eget magna auctor, et feugiat urna mollis. Ut vel est lobortis neque suscipit convallis et ut justo. </p>
-</article><article>
-    <h2>An Article</h2>
-    <p>
-
-        Donec vestibulum aliquet neque, vitae tempor mi sodales quis. Curabitur vel bibendum est, vitae sollicitudin felis. Cras hendrerit bibendum mi sit amet condimentum. Cras consequat metus tellus. Suspendisse ut leo non erat feugiat euismod. Donec ultrices massa a orci rutrum, vel tristique turpis efficitur. Curabitur finibus quis lorem vel facilisis. Mauris sed viverra purus.
-        
-        Nam ac urna tempor, porttitor est sed, sodales lacus. Sed ac nibh eu turpis porta pharetra. Phasellus tempor purus eget nisl ultrices pellentesque. Nulla eget orci et orci placerat tincidunt. Integer ut neque et diam rhoncus ornare. Suspendisse potenti. Pellentesque purus nibh, maximus vel felis eget, mattis condimentum lectus. Praesent eget facilisis nunc. Pellentesque ipsum odio, sollicitudin ac neque ut, hendrerit feugiat mauris. Duis suscipit nulla molestie magna sagittis mattis. Mauris erat justo, dapibus a congue id, tempor mollis est. Integer aliquet, ligula at suscipit ornare, dui nisi fermentum velit, vel tincidunt quam arcu vel lorem. Donec sit amet tincidunt felis. Aenean dui lorem, molestie tempor ultrices eu, feugiat eget ex.
-        
-        Morbi at augue nec nulla tempus semper placerat et nibh. Sed lorem metus, consequat et egestas et, volutpat et libero. Aliquam varius dignissim lobortis. Curabitur posuere mattis mattis. Pellentesque eget eleifend quam. Vestibulum tincidunt nunc eget magna auctor, et feugiat urna mollis. Ut vel est lobortis neque suscipit convallis et ut justo. </p>
-</article><article>
-    <h2>An Article</h2>
-    <p>
-
-        Donec vestibulum aliquet neque, vitae tempor mi sodales quis. Curabitur vel bibendum est, vitae sollicitudin felis. Cras hendrerit bibendum mi sit amet condimentum. Cras consequat metus tellus. Suspendisse ut leo non erat feugiat euismod. Donec ultrices massa a orci rutrum, vel tristique turpis efficitur. Curabitur finibus quis lorem vel facilisis. Mauris sed viverra purus.
-        
-        Nam ac urna tempor, porttitor est sed, sodales lacus. Sed ac nibh eu turpis porta pharetra. Phasellus tempor purus eget nisl ultrices pellentesque. Nulla eget orci et orci placerat tincidunt. Integer ut neque et diam rhoncus ornare. Suspendisse potenti. Pellentesque purus nibh, maximus vel felis eget, mattis condimentum lectus. Praesent eget facilisis nunc. Pellentesque ipsum odio, sollicitudin ac neque ut, hendrerit feugiat mauris. Duis suscipit nulla molestie magna sagittis mattis. Mauris erat justo, dapibus a congue id, tempor mollis est. Integer aliquet, ligula at suscipit ornare, dui nisi fermentum velit, vel tincidunt quam arcu vel lorem. Donec sit amet tincidunt felis. Aenean dui lorem, molestie tempor ultrices eu, feugiat eget ex.
-        
-        Morbi at augue nec nulla tempus semper placerat et nibh. Sed lorem metus, consequat et egestas et, volutpat et libero. Aliquam varius dignissim lobortis. Curabitur posuere mattis mattis. Pellentesque eget eleifend quam. Vestibulum tincidunt nunc eget magna auctor, et feugiat urna mollis. Ut vel est lobortis neque suscipit convallis et ut justo. </p>
-</article>
+<Article>
+    
+</Article>
